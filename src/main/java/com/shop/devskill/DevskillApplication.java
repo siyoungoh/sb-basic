dd09a81bb274ccf234ef3d352651a13d2138ccba
siyongoh <dev@syoh.net>
2023-06-09 00:00:36 +0900

    리팩토링 - GET API annotation 간소화

--- src/main/java/com/shop/devskill/DevskillApplication.java
+++ src/main/java/com/shop/devskill/DevskillApplication.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,8 +33,8 @@ class RestApiDemoController {
 		));
 	}
 
-	@RequestMapping(value = "/skills", method = RequestMethod.GET)
-	Iterable<Skill> getItems() {
+	@GetMapping("/skills")
+	Iterable<Skill> getSkills() {
 		return skills;
 	}
 }

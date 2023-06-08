61f3744b6405bd6e5244f1b6b3c8a115597e3e6b
siyongoh <dev@syoh.net>
2023-06-08 23:44:41 +0900

    추가-spring initializr 생성코드

---
+++ src/main/java/com/shop/devskill/DevskillApplication.java
@@ -0,0 +1,13 @@
+package com.shop.devskill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevskillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevskillApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shop/devskill/DevskillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.devskill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevskillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

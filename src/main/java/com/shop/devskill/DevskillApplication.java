19fff9dd65531ba2ce74dc26d3b78396032fe766
siyongoh <dev@syoh.net>
2023-06-09 01:16:11 +0900

    추가 - Read Id, Create(POST) Update(PUT), Delete(DELETE)
    default mappling url 주석처리함. POST, PUT, DELETE annotation 의 전체 url 이 어떤 건지 보여 주기 위해서.

--- src/main/java/com/shop/devskill/DevskillApplication.java
+++ src/main/java/com/shop/devskill/DevskillApplication.java
@@ -2,12 +2,17 @@ package com.shop.devskill;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -20,9 +25,9 @@ public class DevskillApplication {
 }
 
 @RestController
-@RequestMapping("/skills")
+	// @RequestMapping("/skills")
 class RestApiDemoController {
-	private List<Skill> skills = new ArrayList<>();
+	private final List<Skill> skills = new ArrayList<>();
 
 	public RestApiDemoController() {
 		skills.addAll(List.of(
@@ -33,9 +38,47 @@ class RestApiDemoController {
 		));
 	}
 
-	@GetMapping
+	@GetMapping("/skills")
 	Iterable<Skill> getSkills() {
 		return skills;
+	}
+
+	@GetMapping("/skills/{id}")
+	Optional<Skill> getSkillById(@PathVariable String id) {
+		for (Skill c : skills) {
+			if (c.getId().equals(id)) {
+				return Optional.of(c);
+			}
+		}
+
+		return Optional.empty();
+	}
+
+	@PostMapping("/skills")
+	Skill postSkill(@RequestBody Skill skill) {
+		skills.add(skill);
+		return skill;
+	}
+
+	@PutMapping("/skills/{id}")
+	Skill putSkill(@PathVariable String id,
+		@RequestBody Skill skill) {
+		int skillIndex = -1;
+
+		for (Skill c : skills) {
+			if (c.getId().equals(id)) {
+				skillIndex = skills.indexOf(c);
+				skills.set(skillIndex, skill);
+			}
+		}
+
+		return (skillIndex == -1) ?
+			postSkill(skill) : skill;
+	}
+
+	@DeleteMapping("/skills/{id}")
+	void deleteSkill(@PathVariable String id) {
+		skills.removeIf(c -> c.getId().equals(id));
 	}
 }
 

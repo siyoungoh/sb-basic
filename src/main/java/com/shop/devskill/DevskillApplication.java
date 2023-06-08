d26485b3efdf75eac747db77ad4a38d7eb0d0f74
siyongoh <dev@syoh.net>
2023-06-08 23:46:43 +0900

    추가- skill 클래스

--- src/main/java/com/shop/devskill/DevskillApplication.java
+++ src/main/java/com/shop/devskill/DevskillApplication.java
@@ -1,5 +1,7 @@
 package com.shop.devskill;
 
+import java.util.UUID;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,4 +12,29 @@ public class DevskillApplication {
 		SpringApplication.run(DevskillApplication.class, args);
 	}
 
+}
+class Skill {
+	private final String id;
+	private String name;
+
+	public Skill(String id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Skill(String name) {
+		this(UUID.randomUUID().toString(), name);
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
